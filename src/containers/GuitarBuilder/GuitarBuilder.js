d286d5ae929cdf3d13f0c087341a0f4c8d9ef535
Ted Jenkins <tjenkins@saltstack.com>
2019-10-30 14:46:39 -0600

    initial commit

---
+++ src/containers/GuitarBuilder/GuitarBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class GuitarBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>Guitar</div>
+        <div>Build controls</div>
+      </Fragment>
+    );
+  }
+}
+
+export default GuitarBuilder;
