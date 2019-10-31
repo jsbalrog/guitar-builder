ebe06dc4a32b4596e158ff48d241ee81c5c26d58
Ted Jenkins <tjenkins@saltstack.com>
2019-10-31 10:44:20 -0600

    add eslintrc

--- src/components/Guitar/Guitar.js
+++ src/components/Guitar/Guitar.js
@@ -4,11 +4,26 @@ import classes from './Guitar.module.css';
 import GuitarPart from './GuitarPart/GuitarPart'
 
 const guitar = (props) => {
+  /*
+   *  Given an object like this: { "foo": 2, "bar": 1, "baz": 3},
+   *  return an array ["foo", "foo", "bar", "baz", "baz", "baz"]
+   */
+  let transformedParts = Object.keys(props.parts)
+    .map(pKey => {
+      return [...Array(props.parts[pKey])].map((_, i) => {
+        return <GuitarPart key={pKey + i} type={pKey} />;
+      });
+    }).reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if(transformedParts.length === 0) {
+    transformedParts = <p>Please start adding parts!</p>
+  }
+
   return (
-    <div className={classes.Burger}>
+    <div className={classes.Guitar}>
       <GuitarPart type="bread-top" />
-      <GuitarPart type="cheese" />
-      <GuitarPart type="meat" />
+      {transformedParts}
       <GuitarPart type="bread-bottom" />
     </div>
   );
--- src/containers/GuitarBuilder/GuitarBuilder.js
+++ src/containers/GuitarBuilder/GuitarBuilder.js
@@ -1,12 +1,25 @@
 import React, { Component, Fragment } from "react";
 import Guitar from '../../components/Guitar/Guitar';
+import BuildControls from '../../components/Guitar/BuildControls/BuildControls';
 
 class GuitarBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      parts: {
+        lettuce: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+      }
+    }
+  }
+
   render() {
     return (
       <Fragment>
-        <Guitar />
-        <div>Build controls</div>
+        <Guitar parts={this.state.parts} />
+        <BuildControls />
       </Fragment>
     );
   }
