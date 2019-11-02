d3014bd18b5ba77f19b2b7ac41a5f4ad666f2eb9
Ted Jenkins <tjenkins@saltstack.com>
2019-11-02 13:09:26 -0600

    more responsive adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Fragment>
   <div>
     <Toolbar/>
+    <SideDrawer />
   </div>
   <main className={classes.Content}>
     {props.children}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,8 +8,8 @@ const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <Logo/>
-      <nav>
+      <Logo height="80%" />
+      <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
     </header>
